c6d30d031a475f387b0b797783d7db5b320444be
Mariya Belogolovkina <mariyabelogolovkina@gmail.com>
2024-08-27 05:15:56 +0200

    test: test controller added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TestController } from './test.controller';
 
 @Module({
   imports: [ConfigModule.forRoot()],
-  controllers: [AppController],
+  controllers: [AppController, TestController],
   providers: [AppService],
 })
 export class AppModule {}
