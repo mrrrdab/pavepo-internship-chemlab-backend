d59db42b4dce04492ae9a92eee6868cb7f82b274
Mariya Belogolovkina <mariyabelogolovkina@gmail.com>
2024-08-29 06:40:08 +0200

    misc: test cors sets

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://mrrrda.github.io',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
   });
 
   app.useGlobalPipes(
